65683c6031acbce6c82565032f28ad8ab52c8236
Jolivet-Myliana <myliana.jolivet@edu.univ-fcomte.fr>
2024-12-04 14:33:41 +0100

    fin du JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,14 @@ const x = document.querySelector("#lightbox");
 x.addEventListener("click", function() {
     x.close();
     });
+    const photos = document.querySelectorAll("img");
 
+    photos.forEach(image => {
+        image.addEventListener("click", function() {
+            const fullImg = image.dataset.fullImg;
+            const imgElement = x.querySelector("img");
+            imgElement.src = fullImg;
+            x.showModal();
+        });
+    });
 
